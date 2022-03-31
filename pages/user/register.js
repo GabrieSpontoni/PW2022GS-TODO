214cf3962033dbad7095e440c1187316e0da3fcd
Gabriel Spontoni <gabriel@evop.com>
2022-03-31 09:44:13 -0400

    pw2022gs-1: first commit

---
+++ pages/user/register.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import { useState } from "react/cjs/react.production.min";
+
+export default function Register() {
+  return (
+    <h1>
+      Register
+      <Link href="/">Inicio</Link>
+    </h1>
+  );
+}
