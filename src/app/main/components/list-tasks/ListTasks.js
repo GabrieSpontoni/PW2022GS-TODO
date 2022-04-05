e2b0c5f672daa83486b39fe1612673e5c0e830b5
Gabriel Spontoni <gabriel@evop.com>
2022-04-04 20:46:13 -0400

    pw2022gs-1: create ListsTasks screen

--- src/app/main/components/list-tasks/ListTasks.js
+++ src/app/main/components/list-tasks/ListTasks.js
@@ -17,59 +17,128 @@ export default function Tasks() {
               htmlFor="exampleInputEmail1"
               className="form-label mb-2 text-muted"
             >
-              Nova tarefa
+              Nova Lista
             </label>
             <input type="text" className="form-control" />
           </div>
         </form>
       </div>
-      <label className="form-label mb-2 text-muted">Tarefas</label>
+      <label className="form-label mb-2 text-muted">Listas</label>
+
       <div className={styles.container}>
-        <table className="table">
-          <thead>
-            <tr>
-              <th scope="col">Tarefa</th>
-              <th scope="col">Tempo limite</th>
-              <th scope="col">Tags</th>
-              <th scope="col">Status</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th scope="row">Buscar mãe no mercado</th>
-              <td>20/05/2021</td>
-              <td>
-                <div>
-                  <span className="badge bg-danger">Danger</span>
-                  <span className="badge bg-warning text-dark">Warning</span>
-                </div>
-              </td>
-              <td>Concluído</td>
-            </tr>
-            <tr>
-              <th scope="row">Comprar presente para irmão</th>
-              <td>20/05/2021</td>
-              <td>
-                <div>
-                  <span className="badge bg-danger">Danger</span>
-                  <span className="badge bg-warning text-dark">Warning</span>
-                </div>
-              </td>
-              <td>Não concluído</td>
-            </tr>
-            <tr>
-              <th scope="row">Visitar vovó</th>
-              <td>20/05/2021</td>
-              <td>
-                <div>
-                  <span className="badge bg-danger">Danger</span>
-                  <span className="badge bg-warning text-dark">Warning</span>
-                </div>
-              </td>
-              <td>Atrasado</td>
-            </tr>
-          </tbody>
-        </table>
+        <div className="card">
+          <h5 className="card-header text-muted text-center">Estágio</h5>
+          <div className="table-responsive">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th scope="col">Tarefa</th>
+                  <th scope="col">Tempo limite</th>
+                  <th scope="col">Tags</th>
+                  <th scope="col">Status</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">Consertar bug de usuário</th>
+                  <td>20/05/2021</td>
+                  <td>
+                    <div>
+                      <span className="badge bg-danger">Danger</span>
+                      <span className="badge bg-warning text-dark">
+                        Warning
+                      </span>
+                    </div>
+                  </td>
+                  <td>Concluído</td>
+                </tr>
+                <tr>
+                  <th scope="row">Mudar Layout do login do usuário</th>
+                  <td>20/05/2021</td>
+                  <td>
+                    <div>
+                      <span className="badge bg-danger">Danger</span>
+                      <span className="badge bg-warning text-dark">
+                        Warning
+                      </span>
+                    </div>
+                  </td>
+                  <td>Não concluído</td>
+                </tr>
+                <tr>
+                  <th scope="row">Inserir Vídeo</th>
+                  <td>20/05/2021</td>
+                  <td>
+                    <div>
+                      <span className="badge bg-danger">Danger</span>
+                      <span className="badge bg-warning text-dark">
+                        Warning
+                      </span>
+                    </div>
+                  </td>
+                  <td>Atrasado</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+
+        <div className="card">
+          <h5 className="card-header text-muted text-center">Estágio</h5>
+          <div className="table-responsive">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th scope="col">Tarefa</th>
+                  <th scope="col">Tempo limite</th>
+                  <th scope="col">Tags</th>
+                  <th scope="col">Status</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">Consertar bug de usuário</th>
+                  <td>20/05/2021</td>
+                  <td>
+                    <div>
+                      <span className="badge bg-danger">Danger</span>
+                      <span className="badge bg-warning text-dark">
+                        Warning
+                      </span>
+                    </div>
+                  </td>
+                  <td>Concluído</td>
+                </tr>
+                <tr>
+                  <th scope="row">Mudar Layout do login do usuário</th>
+                  <td>20/05/2021</td>
+                  <td>
+                    <div>
+                      <span className="badge bg-danger">Danger</span>
+                      <span className="badge bg-warning text-dark">
+                        Warning
+                      </span>
+                    </div>
+                  </td>
+                  <td>Não concluído</td>
+                </tr>
+                <tr>
+                  <th scope="row">Inserir Vídeo</th>
+                  <td>20/05/2021</td>
+                  <td>
+                    <div>
+                      <span className="badge bg-danger">Danger</span>
+                      <span className="badge bg-warning text-dark">
+                        Warning
+                      </span>
+                    </div>
+                  </td>
+                  <td>Atrasado</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     </div>
   );
